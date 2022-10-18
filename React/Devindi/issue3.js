6c5e2417a4e700f6c972858ab646295f806d1dda
Devindi Rajapakse <71745965+devindirajapakse@users.noreply.github.com>
2022-10-18 16:26:14 +0530

    Form

---
+++ React/Devindi/issue3.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
